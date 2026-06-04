e941e048a85f08836d68007189efb6978a9f3f7e
pin <151846820+workofthon@users.noreply.github.com>
2026-06-05 02:27:47 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('jangan sedih lagi yaaaaa').split('')
+    const titles = ('semoga Mia senyum lagi!').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
