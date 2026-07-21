427bdac2e0a8a3032fe13dfc544984d043986123
pin <151846820+workofthon@users.noreply.github.com>
2026-07-22 02:59:25 +0700

    Change title message in main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('semoga Mia senyum lagi!').split('')
+    const titles = ('jangan lupa senyum!').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
