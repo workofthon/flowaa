cf2ca37bcd5a90105221afa5d3eac32c4b8160a1
pin <151846820+workofthon@users.noreply.github.com>
2026-08-07 09:23:02 +0700

    Change title text in main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('jangan lupa senyum!').split('')
+    const titles = ('semangat pin :)').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
