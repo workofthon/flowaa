2adadcb661736e3cecebc95da844eddd810d9ad8
pin <151846820+workofthon@users.noreply.github.com>
2026-08-07 09:26:38 +0700

    Update text string in index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = `gift for kevin`.split('')
+const text = `gift 4 kepin`.split('')
 
 // Create container for better responsive layout
 title.style.display = 'flex'
